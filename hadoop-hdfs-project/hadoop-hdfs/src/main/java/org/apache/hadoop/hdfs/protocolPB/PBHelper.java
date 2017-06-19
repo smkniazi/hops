39a025f16be823f033001940c98911149d132dcd
Salman Niazi <smkniazi@kth.se>
2017-06-19 02:42:45 +0200

    Auto generated commit msg.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
@@ -508,11 +508,11 @@ public class PBHelper {
     for (int i = 0; i < locs.length; i++) {
       builder.addLocs(i, PBHelper.convert(locs[i]));
       //For compatability with newer clients
-      builder.setStorageIDs(i, "HopsFS_Hack_Storage_ID" );
-      builder.setStorageTypes(i, HdfsProtos.StorageTypeProto.DISK);
-      builder.setIsCached(i,false);
+      builder.addStorageIDs("HopsFS_Hack_Storage_ID" );
+      builder.addStorageTypes(HdfsProtos.StorageTypeProto.DISK);
+      builder.addIsCached(false);
     }
-    
+
     builder = builder.setB(PBHelper.convert(b.getBlock()))
         .setBlockToken(PBHelper.convert(b.getBlockToken()))
         .setCorrupt(b.isCorrupt()).setOffset(b.getStartOffset());
