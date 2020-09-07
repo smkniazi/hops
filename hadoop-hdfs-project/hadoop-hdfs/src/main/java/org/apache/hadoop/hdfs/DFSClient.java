af5f55785eaff1daff2198dd210d1d996f5b39c8
Salman Niazi <salman@logicalclocks.com>
2020-09-07 11:51:55 +0200

    [HOPS-1587] call refreshNodes on the leader NN

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -2283,7 +2283,7 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
    */
   public void refreshNodes() throws IOException {
     try (TraceScope ignored = tracer.newScope("refreshNodes")) {
-      namenode.refreshNodes();
+      leaderNN.refreshNodes();
     }
   }
 
